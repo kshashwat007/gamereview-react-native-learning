ed1ae769824220584951a02d8eaa2d44e335c560
kshashwat007 <kshashwat007@gmail.com>
2021-04-02 12:43:39 +0400

    Custom Fonts

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './screens/home';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => {
+  return Font.loadAsync({
+    'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+};
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      // Home
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.log('error')}
+      />
+    );
+  }
 }
