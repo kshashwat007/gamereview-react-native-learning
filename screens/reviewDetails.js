23b4fac7fa27cdb2b758ba759b589ab2861d0f5f
kshashwat007 <kshashwat007@gmail.com>
2021-04-03 13:13:03 +0400

    passing data for review screens

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails({ navigation }) {
-  const pressHandler = () => {
-    navigation.goBack('Reviews');
-  };
-
+export default function ReviewDetails({ route, navigation }) {
+  const { title, body, rating } = route.params;
   return (
     <View style={globalStyles.container}>
-      <Text>Review Details Screen</Text>
-      <Button title="Go back" onPress={pressHandler} />
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+      <Text>{rating}</Text>
     </View>
   );
 }
