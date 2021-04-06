2236501aee44aff8d403b5c1d564d49faa9f468c
kshashwat007 <kshashwat007@gmail.com>
2021-04-06 13:08:30 +0400

    card styling

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
 
 export default function ReviewDetails({ route, navigation }) {
   const { title, body, rating } = route.params;
   return (
     <View style={globalStyles.container}>
-      <Text>{title}</Text>
-      <Text>{body}</Text>
-      <Text>{rating}</Text>
+      <Card>
+        <Text>{title}</Text>
+        <Text>{body}</Text>
+        <Text>{rating}</Text>
+      </Card>
     </View>
   );
 }
--- shared/card.js
+++ shared/card.js
@@ -10,6 +10,19 @@ export default function Card(props) {
 }
 
 const styles = StyleSheet.create({
-  card: {},
-  cardContent: {}
+  card: {
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: '#eee',
+    shadowOffset: { width: 1, height: 1 },
+    shadowColor: '#333',
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    marginHorizontal: 4,
+    marginVertical: 6
+  },
+  cardContent: {
+    marginHorizontal: 16,
+    marginVertical: 18
+  }
 });
