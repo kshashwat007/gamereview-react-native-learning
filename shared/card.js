a1b82dabcb34f7943e2df4a17ef2416b45d17db4
kshashwat007 <kshashwat007@gmail.com>
2021-04-06 12:15:58 +0400

    base card component

---
+++ shared/card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Card(props) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>{props.children}</View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {},
+  cardContent: {}
+});
